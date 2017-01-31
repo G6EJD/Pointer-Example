6838c6b3cd0f5c1a11ce2cd6c0ddf79f0c9fac85
G6EJD <d.l.bird@sky.com>
2017-01-31 23:14:49 +0000

    Add files via upload

---
+++ Pointers_Example.ino
@@ -0,0 +1,31 @@
+int myVar;
+float myFloat;
+int * myPtr;
+float * myFPtr;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting test...");
+  myVar = 33;
+  myPtr = &myVar;          // myPtr is the address of the variable myVar
+  Serial.println(*myPtr);  // Print the value pointed to by myPTR
+  *myPtr = 44;             // The variable pointed-to by myPtr = 44
+  Serial.println(*myPtr);  // Print the value pointed to by myPTR
+  myFloat = 22.2;
+  myFPtr  = &myFloat;      // myFPtr is the address of the variable myFloat
+  *myFPtr = 44.4;          // The variable pointed to by myFPtr = 44.4
+  Serial.println(*myFPtr); // Print the value pointed to by myFPTR
+}
+
+void loop() {
+  int Total_Score;
+  Total_Score = 1;
+  totalise(1, 2, &Total_Score);
+  Serial.println("Total is now: "+String(Total_Score));
+  delay(5000);
+}
+
+void totalise(int value1, int value2, int *Data_To_Total){
+  *Data_To_Total = *Data_To_Total + value1 + value2;
+}
+
